58df5e642afc9bdef1fe3ef360b01a9790f6530e
Tsunami Noai <spam@falseblue.com>
2023-12-04 23:08:12 -0500

    fix a typo

--- src/strings.zig
+++ src/strings.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const config = @import("config");
 const english = @import("lang/english.zig").strings;
 const Language = english;
-const french = @import("lang/english.zig").strings;
-const german = @import("lang/english.zig").strings;
+const french = @import("lang/french.zig").strings;
+const german = @import("lang/german.zig").strings;
 
 pub fn Strings(comptime language: anytype) @TypeOf(Language) {
     return switch (language) {
