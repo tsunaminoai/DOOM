f989846ce75deda63f086c98480f8d975b58490d
Tsunami Noai <spam@falseblue.com>
2023-12-07 00:32:13 -0500

    Add system-specific interface module

--- src/libdoom.zig
+++ src/libdoom.zig
@@ -1,26 +1,26 @@
 const std = @import("std");
-pub usingnamespace @import("types.zig");
 pub usingnamespace @import("data.zig");
 pub usingnamespace @import("definitions.zig");
 pub usingnamespace @import("event.zig");
+pub usingnamespace @import("finale.zig");
 pub usingnamespace @import("fixed.zig");
 pub usingnamespace @import("info.zig");
 pub usingnamespace @import("items.zig");
+pub usingnamespace @import("menu.zig");
+pub usingnamespace @import("misc.zig");
 pub usingnamespace @import("mobj.zig");
 pub usingnamespace @import("player.zig");
-pub usingnamespace @import("sprites.zig");
 pub usingnamespace @import("sounds.zig");
+pub usingnamespace @import("sprites.zig");
 pub usingnamespace @import("stat.zig");
+pub usingnamespace @import("system.zig");
 pub usingnamespace @import("tables.zig");
 pub usingnamespace @import("think.zig");
 pub usingnamespace @import("ticks.zig");
-pub usingnamespace @import("wad.zig");
+pub usingnamespace @import("types.zig");
 pub usingnamespace @import("video.zig");
-pub usingnamespace @import("finale.zig");
+pub usingnamespace @import("wad.zig");
 pub usingnamespace @import("wipe.zig");
-pub usingnamespace @import("misc.zig");
-pub usingnamespace @import("menu.zig");
-//todo: "i_system.h"
 //todo: "i_sound.h"
 //todo: "i_video.h"
 //todo: "g_game.h"
---
+++ src/system.zig
@@ -0,0 +1,57 @@
+///	System specific interface stuff.
+const std = @import("std");
+const DOOM = @import("libdoom.zig");
+
+/// Called by DoomMain.
+pub fn init() void {}
+
+/// Called by startup code
+/// to get the ammount of memory to malloc
+/// for the zone management.
+pub fn zoneBase(size: *i16) *u8 {
+    _ = size;
+    return 0;
+}
+
+/// Called by D_DoomLoop,
+/// returns current time in tics.
+pub fn getTime() void {}
+
+/// Called by D_DoomLoop,
+/// called before processing any tics in a frame
+/// (just after displaying a frame).
+/// Time consuming syncronous operations
+/// are performed here (joystick reading).
+/// Can call D_PostEvent.
+pub fn startFrame() void {}
+
+/// Called by D_DoomLoop,
+/// called before processing each tic in a frame.
+/// Quick syncronous operations are performed here.
+/// Can call D_PostEvent.
+pub fn startTick() void {}
+
+/// Asynchronous interrupt functions should maintain private queues
+/// that are read by the synchronous functions
+/// to be converted into events.
+/// Either returns a null ticcmd,
+/// or calls a loadable driver to build it.
+/// This ticcmd will then be modified by the gameloop
+/// for normal input.
+pub fn baseTickCmd() *DOOM.TickCommand {}
+
+/// Called by M_Responder when quit is selected.
+/// Clean exit, displays sell blurb.
+pub fn quit() void {}
+
+/// Allocates from low memory under dos,
+/// just mallocs under unix
+pub fn allocLow(length: i16) *u8 {
+    _ = length;
+}
+
+pub fn tactile(on: i16, off: i16, total: i16) void {
+    _ = total;
+    _ = off;
+    _ = on;
+}
